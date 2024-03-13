f33ec526da6def7805b4690a9ab9295b4b88dd2a
LuisFuturo <luis.futuro.01@gmail.com>
2024-03-13 01:11:12 -0400

    formateando nuevamente todo la pagina

---
+++ php/index.php
@@ -0,0 +1,199 @@
+
+<?php
+$pr1=$POST["pr1"];
+$pr2=$POST["pr2"];
+$pr3=$POST["pr3"];
+$pr4=$POST["pr4"];
+$pr5=$POST["pr5"];
+$pr6=$POST["pr6"];
+$pr7=$POST["pr7"];
+$pr8=$POST["pr8"];
+$pr9=$POST["pr9"];
+$pr10=$POST["pr10"];
+
+$ae=0 , $in=0 , $mc=0 , $gg=0 , $sh=0 , $nt=0 , $cs=0 , $rf=0 , $pi=0 , $ar=0 , $eu=0 , $hi=0 , $en=0;
+
+//pr1//
+if (pr1==a){
+    $ae= $ae+1;     
+    $cs= $cs+1;
+}else if (pr1==b){
+    $in= $in+1;     
+    $rf= $rf+1;
+    $ar= $ar+1;
+    $eu= $eu+1;
+    $en= $en+1;
+}else if (pr1==c){
+    $mc= $mc+1;     
+    $hi= $hi+1;
+}else if (pr1==d){
+    $gg= $gg+1;     
+    $pi= $pi+1;
+}else if (pr1==e){
+    $sh= $sh+1;     
+}else if (pr1==f){
+    $nt= $nt+1;     
+}
+//pr2//
+if (pr2==a){ 
+     $rf= $rf+1;
+    }else if (pr2==b){
+        $pi= $pi+1;
+    }else if (pr2==c){
+    }else if (pr2==d){
+        $cs= $cs+1;
+    }else if (pr2==e){
+        $ae= $ae+1;    
+    }else if (pr2==f){  
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $mc= $mc+1; 
+        $in= $in+1;   
+    }
+   
+    //pr3//
+    if (pr3==a){
+        $pi= $pi+1;
+    }else if (pr2==b){
+    }else if (pr2==c){
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $mc= $mc+1; 
+        $in= $in+1;  
+        $ae= $ae+1;    
+    }else if (pr2==d){
+        $cs= $cs+1;
+    }else if (pr2==e){
+        $rf= $rf+1;
+    }else if (pr2==f){  
+    }
+    //pr4//
+    if (pr4==a){
+    }else if (pr2==b){
+        $cs= $cs+1;
+    }else if (pr2==c){
+      
+    }else if (pr2==d){
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $mc= $mc+1; 
+        $ae= $ae+1;   
+        $in= $in+1;   
+    }else if (pr2==e){
+        $rf= $rf+1;
+    }else if (pr2==f){  
+        $pi= $pi+1;
+    }
+    //pr5//
+    if (pr5==a){
+        $cs= $cs+1;
+    }else if (pr2==b){
+        $rf= $rf+1;
+    }else if (pr2==c){
+    }else if (pr2==d){
+    }else if (pr2==e){
+        $pi= $pi+1;
+    }else if (pr2==f){  
+    }
+    //pr6//
+    if (pr6==a){
+        $pi= $pi+1;
+    }else if (pr2==b){
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $ae= $ae+1;   
+        $mc= $mc+1; 
+        $in= $in+1;   
+    }else if (pr2==c){
+    }else if (pr2==d){
+        $rf= $rf+1;
+    }else if (pr2==e){
+    }else if (pr2==f){  
+        $pi= $pi+1;
+        $cs= $cs+1;
+    }
+    //pr//
+    if (pr7==a){
+        $rf= $rf+1;
+    }else if (pr2==b){
+    }else if (pr2==c){
+        $cs= $cs+1;
+    }else if (pr2==d){
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $mc= $mc+1; 
+        $ae= $ae+1;    
+        $in= $in+1;  
+    }else if (pr2==e){
+    }else if (pr2==f){  
+        $pi= $pi+1;
+    }
+    //pr8//
+    if (pr8==a){
+        $gg= $gg+1;
+        $sh= $sh+1;    
+        $mc= $mc+1; 
+        $ae= $ae+1; 
+        $in= $in+1;     
+    }else if (pr2==b){ 
+        $cs= $cs+1;  
+    }else if (pr2==c){
+        $nt= $nt+1;    
+    }else if (pr2==d){
+    }else if (pr2==e){
+        $rf= $rf+1;
+    }else if (pr2==f){  
+    }
+    //pr9//
+    if (pr9==a){
+        $pi= $pi+1;
+    }else if (pr2==b){
+        $nt= $nt+1;    
+        $sh= $sh+1; 
+        $gg= $gg+1;   
+        $ae= $ae+1;    
+     
+    }else if (pr2==c){
+        $rf= $rf+1;
+    }else if (pr2==d){
+
+        $cs= $cs+1;
+    }else if (pr2==e){
+    }else if (pr2==f){ 
+        $mc= $mc+1;  
+    }
+    //pr10//
+    if (pr10==a){
+        $cs= $cs+1;
+    }else if (pr10==b){
+    }else if (pr10==c){
+        $nt= $nt+1;    
+    }else if (pr10==d){
+    }else if (pr10==e){
+        $pi= $pi+1;
+        $ae= $ae+1;    
+    }else if (pr10==f){  
+        $rf= $rf+1;
+    }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  
+
+
+
+
+?>
